75db9427b97f645d95a8b6e1a10e0110c8cf33ca
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-11-24 14:58:29 +0900

    change slide

--- js/app.js
+++ js/app.js
@@ -4,3 +4,37 @@ const container = document. querySelector('.container');
 hamburger_menu.addEventListener('click', () =>{
     container.classList.toggle('active');
 })
+
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 10,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        on: {
+            slideChangeTransitionStart: function(){
+            $('.swiper-slide-active div').addClass('on');
+            $('.swiper-slide-active p').addClass('on');
+            // $('.swiper-slide.swiper-slide-active h2').css('left', '0px');
+            // $('.swiper-slide.swiper-slide-active .img-group').css('left', '150px');
+            },
+            slideChange: function(){
+            $('.swiper-slide-active div').removeClass('on');
+            $('.swiper-slide-active p').removeClass('on');
+            // $('.swiper-slide.swiper-slide-active h2').css('left', '100%');
+            // $('.swiper-slide.swiper-slide-active .img-group').css('left', '100%');
+            }
+        }
+      });
+});
