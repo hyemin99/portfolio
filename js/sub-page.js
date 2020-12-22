26bfad33b8f486f05207596f9b2665931a138736
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-12-23 00:42:46 +0900

    아래 네브

---
+++ js/sub-page.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('#bottom-nav').css({bottom:'-70px'});
+    var lastScrollTop = 0,
+        delta = 15;
+      $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+
+        if (Math.abs(lastScrollTop-st) <= delta) return;
+        if ((st > lastScrollTop) && (lastScrollTop > 0)) {
+          $('#bottom-nav').css({bottom:'-70px'});
+        } else {
+          $('#bottom-nav').css({bottom:'0'});
+        }
+        lastScrollTop = st;
+      });
+
+      $(window).on('scroll', function() {
+		var scrollTop = window.innerHeight / 2;
+		var eleTop = $('#project-section')[0].getBoundingClientRect().top;
+		if(eleTop > scrollTop) {
+			$('#bottom-nav').css({bottom:'-70px'});
+		}
+		});
+});
